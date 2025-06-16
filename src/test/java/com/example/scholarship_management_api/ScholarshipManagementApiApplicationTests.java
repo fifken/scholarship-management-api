99c528e7ebb5da8e9edae28a3fe1191d82699008
fifken <fifken69@gmail.com>
2025-06-16 12:41:21 +0700

    Initialize

---
+++ src/test/java/com/example/scholarship_management_api/ScholarshipManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scholarship_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarshipManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
