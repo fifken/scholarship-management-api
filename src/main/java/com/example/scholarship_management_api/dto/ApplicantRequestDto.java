455bd9eb6fd8f5c5545608f3c15e40de0626a39f
fifken <fifken69@gmail.com>
2025-06-16 14:54:32 +0700

    create applicantController-applicantService-dtoRequest-dtoRespons

--- src/main/java/com/example/scholarship_management_api/dto/ApplicantRequestDto.java
+++ src/main/java/com/example/scholarship_management_api/dto/ApplicantRequestDto.java
@@ -1,4 +1,24 @@
 package com.example.scholarship_management_api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
 public class ApplicantRequestDto {
+
+    @NotNull
+    @JsonProperty("scholarship_id")
+    private Long scholarshipId;
+
+    @NotBlank
+    @Size(min = 3)
+    @JsonProperty("applicant_name")
+    private String applicantName;
+
+    @DecimalMin("0.00")
+    @DecimalMax("4.00")
+    private Double gpa;
+
+    // Getter & Setter
 }
--- src/main/java/com/example/scholarship_management_api/dto/ApplicantResponseDto.java
+++ src/main/java/com/example/scholarship_management_api/dto/ApplicantResponseDto.java
@@ -1,4 +1,29 @@
 package com.example.scholarship_management_api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
 public class ApplicantResponseDto {
+
+    private Long id;
+
+    @JsonProperty("scholarship_id")
+    private Long scholarshipId;
+
+    @JsonProperty("applicant_name")
+    private String applicantName;
+
+    private Double gpa;
+
+    @JsonProperty("created_at")
+    private LocalDate createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDate updatedAt;
+
+    @JsonProperty("deleted_at")
+    private LocalDate deletedAt;
 }
