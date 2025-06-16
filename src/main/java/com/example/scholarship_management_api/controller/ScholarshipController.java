3a9e7f771c3f0a325db83be7a8eb54d739eaec67
Adenawati Hasim <71482@fif.co.id>
2025-06-16 14:59:13 +0700

    feature/update-scholarship-controller

--- src/main/java/com/example/scholarship_management_api/controller/ScholarshipController.java
+++ src/main/java/com/example/scholarship_management_api/controller/ScholarshipController.java
@@ -1,5 +1,46 @@
 package com.example.scholarship_management_api.controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.scholarship_management_api.model.Applicant;
+import com.example.scholarship_management_api.model.Scholarship;
+import com.example.scholarship_management_api.service.ScholarshipService;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/scholarships")
+@Validated
 public class ScholarshipController {
-    
+
+    private final ScholarshipService scholarshipService;
+
+    public ScholarshipController(ScholarshipService scholarshipService) {
+        this.scholarshipService = scholarshipService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Scholarship> createScholarship(@RequestBody @Validated Scholarship scholarship) {
+        Scholarship savedScholarship = scholarshipService.addScholarship(scholarship);
+        return ResponseEntity.ok(savedScholarship);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Scholarship>> getAllScholarships() {
+        return ResponseEntity.ok(scholarshipService.getAllScholarships());
+    }
+
+    @GetMapping("/{id}/applicants")
+    public ResponseEntity<List<Applicant>> getApplicantsByScholarshipId(@PathVariable Long id) {
+        List<Applicant> applicants = scholarshipService.getApplicantsByScholarshipId(id);
+        return ResponseEntity.ok(applicants);
+    }
 }
